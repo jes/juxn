8669e7daf2845e160edc5f2c7eb17de47db0a74b
James Stanley <james@incoherency.co.uk>
2023-07-16 17:31:35 +0100

    Take ROM filename on command line

--- cmd/juxn/main.go
+++ cmd/juxn/main.go
@@ -8,15 +8,18 @@ import (
 
 func main() {
 	vm := juxn.NewVM()
-	err := vm.LoadROM("tests.rom")
+	if len(os.Args) <= 1 {
+		fmt.Fprintf(os.Stderr, "usage: juxn ROM\n")
+		os.Exit(1)
+	}
+	err := vm.LoadROM(os.Args[1])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
 	vm.Run(1000000)
 	if vm.Halted {
-		fmt.Printf("halted: %s\n", vm.HaltedBecause)
-		fmt.Printf("Wstack = %v\n", vm.WStack)
+		//fmt.Printf("halted: %s\n", vm.HaltedBecause)
 	}
-	fmt.Println("completed!")
+	//fmt.Println("completed!")
 }
